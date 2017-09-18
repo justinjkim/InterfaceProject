748ef9cdd2abf175fd93bfc4fcd287f102132b66
Justin Kim <jjkim928@gmail.com>
2017-09-18 14:38:51 -0400

    added toString method

--- src/com/company/Marine.java
+++ src/com/company/Marine.java
@@ -1,13 +1,21 @@
 package com.company;
 
 public class Marine extends Soldier {
-    String name;
 
-    public Marine (String name) {
+    public Marine (String name, String faction, int powerLevel, boolean isCommander) {
         super(name, "Terran", 1, false);
     }
 
     public String speak() {
         return "Jacked up and good to go!";
+    }
+
+    @Override
+    public String toString() {
+        return "My name is " +
+                name + ", and I'm proud to serve my " +
+                faction + " race.\n" + "Power Level: " +
+                powerLevel + "\nIs Commander? " +
+                isCommander + "\n";
     }
 }
