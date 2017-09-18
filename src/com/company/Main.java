5aedcfe18910b3a371db190e5f9159eef0e138d3
Justin Kim <jjkim928@gmail.com>
2017-09-18 15:20:23 -0400

    testing functionality of hydralisk and protoss instances

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,22 @@ public class Main {
         Marine Jack = new Marine("Jack", "Terran", 1, false);
         System.out.println(Jack);
         System.out.println(Jack.speak());
+        System.out.println(Jack.meleeAttack());
+        System.out.println(Jack.projectileAttack());
+        System.out.println(Jack.kamikazeAttack());
 
         Hydralisk Groucho = new Hydralisk("Groucho", "Zerg", 3, false);
         System.out.println(Groucho);
         System.out.println(Groucho.speak());
+        System.out.println(Groucho.meleeAttack());
+        System.out.println(Groucho.projectileAttack());
+        System.out.println(Groucho.kamikazeAttack());
 
         Archon Buzby = new Archon("Buzby", "Protoss", 5, true);
         System.out.println(Buzby);
         System.out.println(Buzby.speak());
+        System.out.println(Buzby.meleeAttack());
+        System.out.println(Buzby.projectileAttack());
+        System.out.println(Buzby.kamikazeAttack());
     }
 }
