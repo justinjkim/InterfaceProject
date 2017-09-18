8610089ef7dbc601e50b80ff19e917b6adf7ae60
Justin Kim <jjkim928@gmail.com>
2017-09-18 14:38:34 -0400

    testing with the Marine and Hydralisk classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Marine Jack = new Marine("Jack", "Terran", 1, false);
+        System.out.println(Jack);
+        System.out.println(Jack.speak());
+
+        Hydralisk Groucho = new Hydralisk("Groucho", "Zerg", 3, false);
+        System.out.println(Groucho);
+        System.out.println(Groucho.speak());
     }
 }
