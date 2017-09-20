33658727596691f8da22b8861637b798eae16711
Justin Kim <jjkim928@gmail.com>
2017-09-19 23:50:31 -0400

    added default punch method

--- src/com/company/MeleeAttack.java
+++ src/com/company/MeleeAttack.java
@@ -2,4 +2,8 @@ package com.company;
 
 public interface MeleeAttack {
     public String meleeAttack();
+
+    default String punch() {
+        return "My fists are the only weapons I need.";
+    }
 }
